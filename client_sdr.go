4563ed65c9b7f12465551aeaa2f66e213a366bc6
董俊强 <strideahead@126.com>
2017-07-04 10:58:22 +0800

    分拆函数,返回依然是sDRRecordAndValue

--- client_sdr.go
+++ client_sdr.go
@@ -3,6 +3,7 @@ package ipmi
 import (
 	"bytes"
 	"errors"
+	//"fmt"
 	"math"
 )
 
@@ -39,7 +40,6 @@ func (c *Client) GetSDR(reservationID uint16, recordID uint16) (sdr *sDRRecordAn
 		},
 	}
 	recordKeyBody_Data := new(bytes.Buffer)
-	fmt.Println(fmt.Println("type======", reflect.TypeOf(recordKeyBody_Data)))
 	res_step1 := &GetSDRCommandResponse{}
 	c.Send(req_step1, res_step1)
 	readData_step1 := res_step1.ReadData
@@ -59,40 +59,20 @@ func (c *Client) GetSDR(reservationID uint16, recordID uint16) (sdr *sDRRecordAn
 	res_step2 := &GetSDRCommandResponse{}
 	c.Send(req_step2, res_step2)
 	recordKeyBody_Data.Write(res_step2.ReadData)
-	return nil, res_step2.NextRecordID
+	sdrRecordAndValue := c.CalSdrRecordValue(recordType, recordKeyBody_Data)
+	return sdrRecordAndValue, res_step2.NextRecordID
 }
-func (c *Client) CalSdrRecordValue(recordType uint8, buffer *Buffer) {
-
-	var result float64
+func (c *Client) CalSdrRecordValue(recordType uint8, recordKeyBody_Data *bytes.Buffer) *sDRRecordAndValue {
 	if recordType == SDR_RECORD_TYPE_FULL_SENSOR {
 		//Unmarshalbinary and assert
 		fullSensor, _ := NewSDRFullSensor(0, "")
 		var sdrRecordAndValue = &sDRRecordAndValue{}
 		fullSensor.UnmarshalBinary(recordKeyBody_Data.Bytes())
 		sdrRecordAndValue.SDRRecord = fullSensor
-		//threshold type
-		if fullSensor.ReadingType == SENSOR_READTYPE_THREADHOLD {
-			// has analog value
-			if fullSensor.Unit&0xc0 != 0xc0 {
-				m, b, bexp, rexp := fullSensor.GetMBExp()
-				sensorReading, err := c.GetSensorReading(fullSensor.SensorNumber)
-				if err != nil {
-					sdrRecordAndValue.avail = false
-					sdrRecordAndValue.value = 0.0
-				} else {
-					switch (fullSensor.Unit & 0xc0) >> 6 {
-					case 0:
-						result = (float64(m)*float64(sensorReading) + float64(b)*math.Pow(10, float64(bexp))) * math.Pow(10, float64(rexp))
-					case 1:
-					case 2:
-						result = (float64(int8(m)*int8(sensorReading)) + float64(b)*math.Pow(10, float64(rexp))) * math.Pow(10, float64(bexp))
-					}
-					sdrRecordAndValue.avail = true
-					sdrRecordAndValue.value = result
-				}
-			}
-		}
-		return sdrRecordAndValue, res_step2.NextRecordID
+		res, avai := c.CalFullSensorValue(fullSensor)
+		sdrRecordAndValue.avail = avai
+		sdrRecordAndValue.value = res
+		return sdrRecordAndValue
 	} else if recordType == SDR_RECORD_TYPE_COMPACT_SENSOR {
 		//Unmarshalbinary and assert
 		var sdrRecordAndValue = &sDRRecordAndValue{}
@@ -113,8 +93,36 @@ func (c *Client) CalSdrRecordValue(recordType uint8, buffer *Buffer) {
 				}
 			}
 		}
-		return sdrRecordAndValue, res_step2.NextRecordID
+		return sdrRecordAndValue
 	}
+	return nil
+}
+
+func (c *Client) CalFullSensorValue(fullSensor *SDRFullSensor) (float64, bool) {
+	var result float64
+	var avail bool
+	//threshold type
+	if fullSensor.ReadingType == SENSOR_READTYPE_THREADHOLD {
+		// has analog value
+		if fullSensor.Unit&0xc0 != 0xc0 {
+			m, b, bexp, rexp := fullSensor.GetMBExp()
+			sensorReading, err := c.GetSensorReading(fullSensor.SensorNumber)
+			if err != nil {
+				result = 0.0
+				avail = false
+			} else {
+				switch (fullSensor.Unit & 0xc0) >> 6 {
+				case 0:
+					result = (float64(m)*float64(sensorReading) + float64(b)*math.Pow(10, float64(bexp))) * math.Pow(10, float64(rexp))
+				case 1:
+				case 2:
+					result = (float64(int8(m)*int8(sensorReading)) + float64(b)*math.Pow(10, float64(rexp))) * math.Pow(10, float64(bexp))
+				}
+				avail = true
+			}
+		}
+	}
+	return result, avail
 }
 
 //Get Sensor Reading  35.14
